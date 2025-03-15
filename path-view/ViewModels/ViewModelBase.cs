89a442dcc5ed908bdf78d9277a4ad5e5b19b9286
John <jbhumph@gmail.com>
2025-03-15 12:28:37 -0700

    Initial commit

---
+++ path-view/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace path_view.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ path-view/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace path_view.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
